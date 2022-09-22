d14da1213aee7b22676b17c5900143639f42edfa
Vladimir Yurlov <yurlov43@gmail.com>
2022-09-22 22:17:30 +0300

    Added Ship class

---
+++ src/main/java/org/example/Ship.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Ship  extends Transport{
+    public Ship(String name, int capacity, int speed, float costOfKm) {
+        super(name, capacity, speed, costOfKm);
+    }
+
+    public float getPrice(City city){
+        return city.isOnWater() ? city.getDistanceKm() * getCostOfKm() : 0;
+    }
+}
