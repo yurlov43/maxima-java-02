446fc86041e05869b763d90653412f11cb9dcf85
Vladimir Yurlov <yurlov43@gmail.com>
2022-09-22 22:16:14 +0300

    Added Truck class

---
+++ src/main/java/org/example/Truck.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Truck extends Transport{
+    public Truck(String name, int capacity, int speed, float costOfKm) {
+        super(name, capacity, speed, costOfKm);
+    }
+}
