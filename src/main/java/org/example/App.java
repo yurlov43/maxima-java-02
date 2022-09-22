38351bc53e79efe279b72599ac7480fcd87714c7
Vladimir Yurlov <yurlov43@gmail.com>
2022-09-22 22:19:33 +0300

    Test use Plane, Truck and Ship classes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,11 @@ public class App
     public static void main( String[] args ){
         City kirov = new City("Kirov", 300);
         Transport auto = new Transport("auto", 40, 100, 10.5f);
-        System.out.print(auto.getPrice(kirov));
+        Truck truck = new Truck("truck", 12, 65, 34.6f);
+        Ship ship = new Ship("ship", 23, 43, 45.6f);
+        Plane plane = new Plane("plane", 54, 23, 65.6f);
+        System.out.println(truck.getPrice(kirov));
+        System.out.println(ship.getPrice(kirov));
+        System.out.println(plane.getPrice(kirov));
     }
 }
