5e83055de5e5ca83ee1dce9eeba8182195d7b32e
Vladimir Yurlov <yurlov43@gmail.com>
2022-09-22 22:14:50 +0300

    Added properties hasAirport and isOnWater to City class

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -3,10 +3,18 @@ package org.example;
 public class City {
     private String name;
     private int distanceKm;
+    private boolean hasAirport;
+    private boolean isOnWater;
 
     public City(String name, int distanceKm) {
+        this(name, distanceKm, false, true);
+    }
+
+    public City(String name, int distanceKm, boolean hasAirport, boolean isOnWater) {
         this.name = name;
         this.distanceKm = distanceKm;
+        this.hasAirport = hasAirport;
+        this.isOnWater = isOnWater;
     }
 
     public String getName() {
@@ -23,5 +31,21 @@ public class City {
 
     public void setDistanceKm(int distanceKm) {
         this.distanceKm = distanceKm;
+    }
+
+    public boolean isHasAirport() {
+        return hasAirport;
+    }
+
+    public void setHasAirport(boolean hasAirport) {
+        this.hasAirport = hasAirport;
+    }
+
+    public boolean isOnWater() {
+        return isOnWater;
+    }
+
+    public void setOnWater(boolean onWater) {
+        isOnWater = onWater;
     }
 }
