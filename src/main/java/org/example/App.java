9d5d60df6f15fbd771fda59e53c33143097b4bb8
Vladimir Yurlov <yurlov43@gmail.com>
2022-10-04 12:37:27 +0300

    The search for the optimal vehicle has been tested

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,8 +7,14 @@ public class App
         Truck truck = new Truck("truck", 12, 65, 34.6f);
         Ship ship = new Ship("ship", 23, 43, 45.6f);
         Plane plane = new Plane("plane", 54, 23, 65.6f);
-        System.out.println(truck.getPrice(kirov));
-        System.out.println(ship.getPrice(kirov));
-        System.out.println(plane.getPrice(kirov));
+        Logistics logistics = new Logistics(truck, ship, plane);
+        ship.startRepair();
+        ship.finishRepair();
+        Transport suitableVehicle = logistics.getShipping(kirov, 10, 5);
+        if(suitableVehicle != null) {
+            System.out.println(suitableVehicle.getName());
+        } else {
+            System.out.println("There is no suitable vehicle!");
+        }
     }
 }
